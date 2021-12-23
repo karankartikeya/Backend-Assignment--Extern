cc555d454254e45b2d00df0fa60578e8c80d6769
unknown <karankartikey72@gmail.com>
2021-12-23 12:11:20 +0530

    starter files for the frontend server

--- backend/youtube_data_api/youtube_data_api/settings.py
+++ backend/youtube_data_api/youtube_data_api/settings.py
@@ -11,16 +11,20 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+#Initialise environment variables
+env = environ.Env()
+environ.Env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q4to&m#gs_!y4mawwvwi_u^ii=q2^f%p%ymk0!jgdibfqa$3-*'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
